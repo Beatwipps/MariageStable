a8112ccdbca49585e921fc47fc9b3f9b5d5597bf
Beatwipps <jordann.perrotta@gmail.com>
2017-03-18 15:22:48 +0100

    Réctification des noms des fichiers de test

--- src/main/Client.java
+++ src/main/Client.java
@@ -15,14 +15,14 @@ public class Client {
 		List<String> listeDes_A;
 		List<String> listeDes_B;
 		
-		LireFichier fichierFemmeBasic = 
-				new LireFichier("./src/main/AffectationFemmeBasic.txt");
-		LireFichier fichierHommeBasic = 
-				new LireFichier("./src/main/AffectationHommeBasic.txt");
-		preferenceDes_B = fichierFemmeBasic.getMap();	
-		preferenceDes_A = fichierHommeBasic.getMap();
-		listeDes_A = fichierHommeBasic.getList();
-		listeDes_B = fichierFemmeBasic.getList();
+		LireFichier fichierBasic_B = 
+				new LireFichier("./src/main/AffectationBasic_B.txt");
+		LireFichier fichierBasic_A = 
+				new LireFichier("./src/main/AffectationBasic_A.txt");
+		preferenceDes_B = fichierBasic_B.getMap();	
+		preferenceDes_A = fichierBasic_A.getMap();
+		listeDes_A = fichierBasic_A.getList();
+		listeDes_B = fichierBasic_B.getList();
 		StableStrategie B = new BasicStable();
 		Instance I1 = new Instance(B, preferenceDes_A, preferenceDes_B, listeDes_A, listeDes_B);
 		I1.executeStableStrategie();
